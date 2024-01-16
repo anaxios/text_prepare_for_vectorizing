524c6cc53290b294bdeca7132b46d64d9192047b
anaxios <skybosh.xyz@outlook.com>
2024-01-16 14:42:15 +0000

    Refactor code for extracting and formatting paragraphs

--- checkhash.py
+++ checkhash.py
@@ -3,7 +3,7 @@ import sys
 
 def hash_string(input_string):
     sha_signature = hashlib.sha256(input_string.encode()).hexdigest()
-    print(sha_signature)
+    print(sha_signature[:31])
 
 if __name__ == "__main__":
     hash_string(sys.argv[1])
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from importlib import metadata
 import re
 import json
 import hashlib
@@ -10,37 +11,13 @@ working_directory = os.getcwd()
 
 def hash_string(input_string):
     sha_signature = hashlib.sha256(input_string.encode()).hexdigest()
-    return sha_signature
+    return sha_signature[:31]
 
 
 
-class ParagraphFormatter:
-    def __init__(self, file_path):
-        with open(file_path, 'r') as file:
-            content = file.read()
-        paragraphs = content.split('\n\n')  # split by blank lines
-        # paragraphs = content.split('__________________________________________________________________')  # split by pages
-        self.formatted_paragraphs = [[{"pageContent": p, "metadata": {}}] for p in paragraphs]
-        self.ids = [{"id": [hash_string(i)]} for i in paragraphs]
-        self.index = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index >= len(self.formatted_paragraphs):
-            raise StopIteration
-        result = self.formatted_paragraphs[self.index], self.ids[self.index]
-        self.index += 1
-        return result
-
-
-def extract_references_from_file(filename):
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-
+def extract_references_from_paragraphs(paragraphs):
     references = {}
-    for line in lines:
+    for line in paragraphs:
         match = re.search(r'\[(\d+)\]', line)
         if match:
             key = int(match.group(1))  # Convert the string to an integer
@@ -49,68 +26,96 @@ def extract_references_from_file(filename):
 
     return references
 
-def replace_bracketed_numbers_with_dict_values(text, dictionary):
-    def replacer(match):
-        key = int(match.group(1))  # Convert the string to an integer
-        return f"[{dictionary.get(key, match.group(0))}]"  # If the key exists in the dictionary, replace it. Otherwise, leave it as is.
-
-    return re.sub(r'\[(\d+)\]', replacer, text)
+def replace_bracketed_numbers_with_dict_values(dictionary):
+    def with_text(text):
+        def replacer(match):
+            key = int(match.group(1))  # Convert the string to an integer
+            return f"[{dictionary.get(key, match.group(0))}]"  # If the key exists in the dictionary, replace it. Otherwise, leave it as is.
+        return re.sub(r'\[(\d+)\]', replacer, text)
+    return with_text
 
 def extract_paragraphs(bookname):
-    # with open(bookname, 'r') as file:
-    #     content = file.read()
     paragraphs = bookname.split('\n\n')
-    paragraphs = [re.sub(' +', ' ', paragraph.replace('\n', ' ')).strip() for paragraph in paragraphs]
     return paragraphs
 
+def remove_line_from_string(s):
+    return re.sub('_{3,}', '', s)
 
-    
+def trim_whitespace(s):
+    return re.sub(' +', ' ', s).strip()
 
-def remove_paragraphs_with_bracketed_numbers(paragraphs):
-    return [paragraph for paragraph in paragraphs if not re.match(r'^\[\d+\].*', paragraph)]
+def replace_newlines_with_spaces(s):
+    return re.sub('\n', ' ', s)
 
-def remove_fileurls(paragraphs):
-    return [paragraph for paragraph in paragraphs if not re.match(r'file:', paragraph)]
+def remove_reference_lines_from_paragraph(paragraph):
+    return None if re.match(r'^\[\d+\].*', paragraph) else paragraph
 
-def split_array(arr, delimiter):
-    result = []
-    temp = []
-    for i in arr:
-        if i == delimiter:
-            result.append(temp)
-            temp = []
-        else:
-            temp.append(i)
-    if temp:
-        result.append(temp)
-    return result
+def remove_paraphraphs_shorter_than(minimum):
+        def with_paragraph(paragraph):
+            temp = paragraph.split(' ')
+            return None if len(temp) <= minimum else paragraph
+        return with_paragraph
+
+def transform_paragraphs(paragraphs, algorithm):
+    temp = [algorithm(paragraph) for paragraph in paragraphs]
+    return [paragraph for paragraph in temp if paragraph]
+
+def match_paragraphs_to_hashes(paragraphs, hash_function=hash_string):
+    dict = {}
+    for paragraph in paragraphs:
+        key = hash_function(paragraph)
+        dict[key] = paragraph
+    return dict
+
+class Paragraph_format_to_vector_json:
+    def __init__(self, paragraphs, metadata):
+        self.index = 0
+        self.formatted_paragraphs = []
+        self.ids = []
+
+        for key, val in paragraphs.items():
+            self.formatted_paragraphs.append({"pageContent": val, "metadata": metadata })
+            self.ids.append({"ids": [key]})
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.formatted_paragraphs):
+            raise StopIteration
+        result = [self.formatted_paragraphs[self.index]], self.ids[self.index]
+        self.index += 1
+        return result
 
 def main(file_path):
     filename = os.path.basename(file_path)
 
     with open(file_path, 'r') as file:
         content = file.read()
-    references = extract_references_from_file(file_path)
+
     paragraphs = extract_paragraphs(content)
-    paragraphs = remove_paragraphs_with_bracketed_numbers(paragraphs)
-    paragraphs = [replace_bracketed_numbers_with_dict_values(paragraph, references) for paragraph in paragraphs]
-    # paragraphs = [remove_fileurls(paragraph) for paragraph in paragraphs]
-    
-    temp_file = f'{working_directory}/tmp/{filename}_tmp'
 
-    with open(temp_file, 'a') as file:  # Open the file in append mode
-        for i, book in enumerate(paragraphs):
-            file.write(book)
-            file.write('\n\n')  # Append double newlines after each element
+    paragraphs = transform_paragraphs(paragraphs, replace_newlines_with_spaces)
+    paragraphs = transform_paragraphs(paragraphs, remove_line_from_string)
+    paragraphs = transform_paragraphs(paragraphs, trim_whitespace)
 
-    formatter = ParagraphFormatter(temp_file)
+    # now that it's cleaned up, we can extract the references
+    references = extract_references_from_paragraphs(paragraphs)
+
+    paragraphs = transform_paragraphs(paragraphs, remove_reference_lines_from_paragraph)
+    paragraphs = transform_paragraphs(paragraphs, replace_bracketed_numbers_with_dict_values(references))
+    paragraphs = transform_paragraphs(paragraphs, remove_paraphraphs_shorter_than(12))
+
+    paragraphs_hashes = match_paragraphs_to_hashes(paragraphs)
+
+    paragraphs = Paragraph_format_to_vector_json(paragraphs_hashes, {"filename": filename})
 
 
     headers = {
         'Authorization': 'Bearer yourmom',
     }
 
-    for result in formatter:
+    for result in paragraphs:
         response = requests.post("https://langchain-workers.derelict.workers.dev/input", headers=headers, json=result)
         print(f"{response.status_code} + {json.dumps(result)}")
         # time.sleep(1)
@@ -118,7 +123,6 @@ def main(file_path):
 
 
 
-main(sys.argv[1])
 
 
 if __name__ == "__main__":
