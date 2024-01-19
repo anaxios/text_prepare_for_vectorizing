c4b0076255aeb007a1c8512840a4fa983f075097
anaxios <skybosh.xyz@outlook.com>
2024-01-19 15:00:51 +0000

    Refactor bookTitle function and add chapterRecur helper function

--- parser.ts
+++ parser.ts
@@ -22,20 +22,35 @@ const baseName = path.basename(filePath, '.json');
 const authors = json.ThML[`ThML.body`].div1.map(e => e.h1.span).filter(Boolean);
 const books = json.ThML[`ThML.body`].div1.map(e => e.div2);
 
+const hTag = new RegExp('h[1-9]');
+const divTag = new RegExp('div[1-9]');
 
+const testKey = (pattern: RegExp) => (key: string) => pattern.test(key) && key;
 
+const H = testKey(hTag);
+const DIV = testKey(divTag);
+
+const chapterRecur = (section: object[]) => ([...section[DIV('div3')].map(f => ({
+                                        chapter: (typeof f.h3 === 'object') 
+                                            ? f.h3['#text'] 
+                                            : f.h3
+                                        ,body: Array.isArray(f.p) 
+                                            ? f.p.map(e => (e['#text'])).filter(Boolean) 
+                                            : f.p['#text']
+                                    }))]);
 
 
 const bookTitle = (n: number) => books[n]
-                                .map(e => e.div3 && Array.isArray(e.div3) ? ({
-                                    book: (typeof e.h2 === 'object') ? e.h2['#text'] : e.h2
-                                    ,author: authors[ n - 1 ]
-                                    ,chapters: [...e.div3.map(e => ({
-                                        chapter: (typeof e.h3 === 'object') ? e.h3['#text'] : e.h3
-                                        ,body: Array.isArray(e.p) ? e.p.map(e => (e['#text'])).filter(Boolean) : e.p['#text']
-                                    }))]
-  
-                                }): null)
+                                .map(e => e.div3 && Array.isArray(e.div3) 
+                                    ? ({
+                                        book: (typeof e.h2 === 'object') 
+                                        ? e.h2['#text'] 
+                                        : e.h2
+                                        ,author: authors[ n - 1 ]
+                                        ,chapters: chapterRecur(e)
+    
+                                    })
+                                    : null)
                                 .filter(Boolean)
                                 .flat()
                                 
@@ -49,9 +64,9 @@ const bookTitle = (n: number) => books[n]
 // console.log(authors);
 
 
-const formatted = bookTitle(7).map(e => (e.chapters.map(f => ({
+const formatted = bookTitle(3).map(e => (e.chapters.map(f => ({
     pageContent: (Array.isArray(f.body)) ? f.body.join(' ') : f.body
-    ,metadata: {Author: e.author, Book: e.book, Chapter: f.title, Volume: baseName }
+    ,metadata: {Author: e.author, Book: e.book, Chapter: f.chapter, Volume: baseName }
 })))).flat();
 
 const ids = { ids: formatted.map(e => e.pageContent ? getSHA256Hash(e.pageContent) : "no content")};
@@ -61,3 +76,6 @@ const res = [formatted, ids];
 await Bun.write('text.json', JSON.stringify(res, null, 2));
 console.log(util.inspect(res, { depth: null, colors: true }));
 console.log(util.inspect(authors, { depth: null, colors: true }));
+
+console.log(H('hr'));
+console.log(DIV('div'));
